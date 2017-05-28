7f80c2d4b0b80dbb43fb70915a0a60decf655814
AkshayB1991 <akshaybhandareab4@gmail.com>
2017-05-28 20:31:19 +0530

    FirstCommit

---
+++ CRUD/CRUDApplicationBO/EmployeeBO.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRUDApplicationDO;
+using System.Data;
+using EntitiesVO;
+
+namespace CRUDApplicationBO
+{
+    public class EmployeeBO
+    {
+        EmployeeDO objEmployeeDO;
+        public DataTable GetEmployeeDetails(EmployeeVO objEmployeeVO)
+        {
+            objEmployeeDO = new EmployeeDO();
+            return objEmployeeDO.GetEmployeeDetails(objEmployeeVO);
+        }
+
+        public void EditEmployeeDetails(EmployeeVO objEmployeeVO)
+        {
+            objEmployeeDO = new EmployeeDO();
+            objEmployeeDO.EditEmployeeDetails(objEmployeeVO);
+        }
+
+        public EmployeeVO GetEditEmployeeDetails(EmployeeVO objEmployeeVO)
+        {
+            objEmployeeDO = new EmployeeDO();
+            return objEmployeeDO.GetEditEmployeeDetails(objEmployeeVO);
+        }
+
+        public DataTable GetDesignationDetails()
+        {
+            objEmployeeDO = new EmployeeDO();
+            return objEmployeeDO.GetDesignationDetails();
+        }
+
+        public DataTable GetDepratment()
+        {
+            objEmployeeDO = new EmployeeDO();
+            return objEmployeeDO.GetDepartmentDetails();
+        }
+
+        public void AddEmployeeDetails(EmployeeVO objEmployeeVO)
+        {
+            objEmployeeDO = new EmployeeDO();
+            objEmployeeDO.AddEmployeeDetails(objEmployeeVO);
+        }
+
+        public List<EmployeeVO> GetAllEmployeeDetails(EmployeeVO objEmployeeVO)
+        {
+            objEmployeeDO = new EmployeeDO();
+            return objEmployeeDO.GetAllEmployeeDetails(objEmployeeVO);
+        }
+
+        public void DeleteEmployee(EmployeeVO objEmployeeVO)
+        {
+            objEmployeeDO = new EmployeeDO();
+            objEmployeeDO.DeleteEmployee(objEmployeeVO);
+        }
+
+        public List<FruitsVO> GetAllFruits()
+        {
+            objEmployeeDO = new EmployeeDO();
+            return objEmployeeDO.GetAllFruits();
+        }
+    }
+}
